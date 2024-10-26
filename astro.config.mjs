0b8df8070f76051a41b7d92120a6158e22cecd32
Hieu Tran <hieu.t.tran4@boeing.com>
2024-10-26 22:06:04 +1000

    Add workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 import pagefind from "astro-pagefind";
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://hieutran106.github.io',
+    base: 'lmao',
     build: {
         format: "file"
     },
